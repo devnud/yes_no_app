7c3213730093608f1345252b18616e4c1def2e4f
devnud <taximadridavid@gmail.com>
2023-11-19 00:44:29 +0100

    creando ChatScreen

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://scontent-mad2-1.xx.fbcdn.net/v/t1.6435-9/197868907_4277868055609315_4215524690463680894_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=be3454&_nc_ohc=70OF0tKIEzQAX8VoMze&_nc_ht=scontent-mad2-1.xx&oh=00_AfD1vHVLPywUeJeTkQXbtQvHDaTBR21zP-DUEznu3Ynayw&oe=6580C0D8',
+            ),
+          ),
+        ),
+        title: const Text('Mi  amor ❤'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
